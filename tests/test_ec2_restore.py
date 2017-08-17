9ea19719cf9103eccb8e2ff2de2f6fcd74ce3c3f
Tobias Witek <tobi@tobi-wan-kenobi.at>
2017-08-17 14:16:51 +0200

    [tests/ec2-restore] Add more tests for restoring EC2 checkpoints

--- tests/test_ec2_restore.py
+++ tests/test_ec2_restore.py
@@ -12,6 +12,7 @@ class TestEc2Restore(unittest.TestCase):
         self.boto3 = mock_boto3.Boto3Mock()
 
         self.runningInstanceId = "i-234123432"
+        self.someInstanceId = "i-abcdef"
         self.someSnapshotId = "snap-123"
         tags = [
                 {"Key": "timewarp:checkpoint_id", "Value": self.someSnapshotId},
@@ -24,9 +25,13 @@ class TestEc2Restore(unittest.TestCase):
         }]
 
         self.runningVM = timewarp.ec2.VirtualMachine(self.runningInstanceId)
-        self.boto3.Instance(self.runningInstanceId).state = { "Name": "running", "Code": 16 }
+        self.boto3.Instance(self.runningInstanceId).state = {"Name": "running", "Code": 16}
         self.boto3.Instance(self.runningInstanceId).block_device_mappings = []
         self.runningCheckpoint = self.runningVM.list_checkpoints()[0]
+
+        self.someVM = timewarp.ec2.VirtualMachine(self.someInstanceId)
+        self.boto3.Instance(self.someInstanceId).state = {"Name": "stopped"}
+        self.boto3.Instance(self.someInstanceId).block_device_mappings = []
 
     def tearDown(self):
         self.boto3.teardown()
@@ -40,7 +45,7 @@ class TestEc2Restore(unittest.TestCase):
             self.runningVM.restore_checkpoint(self.runningCheckpoint.id, force=True)
 
     def test_failIfNoDeviceSpecification(self):
-        self.runningVM.block_device_mappings = [{"DeviceName": "xvda", "VolumeId": "vol-1234"}]
+        self.boto3.Instance(self.runningInstanceId).block_device_mappings = [{"DeviceName": "xvda", "VolumeId": "vol-1234"}]
 
         with self.assertRaises(RuntimeError):
             self.runningVM.restore_checkpoint(self.runningCheckpoint.id, force=True)
@@ -54,16 +59,54 @@ class TestEc2Restore(unittest.TestCase):
         })})
         self.boto3.client().create_volume.return_value = {"VolumeId": "vol-1234"}
 
-        self.boto3.Volume(self.runningInstanceId).state = { "Name": "running", "Code": 16 }
-
         self.runningVM.restore_checkpoint(self.runningCheckpoint.id, force=True)
         vm.stop.assert_called_once()
         self.boto3.client().create_volume.assert_called_once()
+        vm.start.assert_called_once()
 
+    def create_checkpoint(self, instance_id, checkpoint_id, snapshot_count, start_dev = "a"):
+        snapshots = []
 
-#
-#        # ensure that for each volume, a new volume gets created
-#        # ensure each volume is deleted
-#        # ensure each snapshot volume gets created and attached
+        for i in range(snapshot_count):
+            tags = [
+                    {"Key": "timewarp:checkpoint_id", "Value": checkpoint_id},
+                    {"Key": "timewarp:instance", "Value": instance_id},
+                    {"Key": "timewarp:device", "Value": "/dev/xvd{}".format(chr(ord(start_dev) + i)) },
+                    {"Key": "timewarp:device_specs", "Value": json.dumps({
+                        "AvailabilityZone": "us-east-1", "Encrypted": False, "Iops": 100, "KmsKeyId": None,
+                        "Size": 10, "VolumeType": "gp2", "Tags": []
+                    })}
+
+            ]
+            snapshots.append({
+                "StartTime": datetime.datetime.now(), "Tags": tags, "SnapshotId": "snap-{}".format(i)
+            })
+
+        self.boto3.paginator().paginate.return_value = [{
+            "Snapshots": snapshots
+        }]
+
+    def test_multiRestore(self):
+        volumes = []
+        char = "a"
+        volume_count = 3
+        self.create_checkpoint(self.someInstanceId, "some-checkpoint-id", volume_count)
+        inst = self.boto3.Instance(self.someInstanceId)
+        for i in range(volume_count):
+            volumes.append(self.boto3.Volume("vol-{}".format(i)))
+            inst.block_device_mappings.append({
+                "DeviceName": "/dev/xvd{}".format(char), "Ebs": { "VolumeId": volumes[i].id }
+            })
+            char = chr(ord(char)+1)
+
+        self.boto3.client().create_volume.return_value = {"VolumeId": "vol-1234"}
+        self.someVM.restore_checkpoint("some-checkpoint-id")
+
+        for v in volumes:
+            v.delete.assert_called_once()
+        self.boto3.client().create_volume.assert_called()
+        # TODO: validate parameters
+        self.assertEquals(volume_count, len(self.boto3.client().create_volume.call_args_list))
+        self.assertEquals(volume_count, len(inst.attach_volume.call_args_list))
 
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
