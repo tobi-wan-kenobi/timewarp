4950537a42f2b403faf44d77f400746516fd6a4e
Tobias Witek <tobi@tobi-wan-kenobi.at>
2017-08-17 11:53:17 +0200

    [tests/ec2-create] Add tests for multi-volume snapshot creation
    * Check that EBS volumes are ignored (probably not the most secure
    checks right now, but unable to come up with something better)
    * Check for device tag in timewarp tags

--- tests/test_ec2_create.py
+++ tests/test_ec2_create.py
@@ -33,6 +33,16 @@ class TestEc2Create(unittest.TestCase):
         self.assertTrue({"Key": "timewarp:instance", "Value": self.someInstanceId} in self.snapshot.create_tags.call_args_list[0][1]["Tags"])
         self.assertTrue({"Key": "timewarp:checkpoint_id", "Value": snap.id} in self.snapshot.create_tags.call_args_list[0][1]["Tags"])
         self.assertTrue({"Key": "timewarp:volume_id", "Value": self.volume.id} in self.snapshot.create_tags.call_args_list[0][1]["Tags"])
+        self.assertTrue({"Key": "timewarp:device", "Value": "/dev/sda"} in self.snapshot.create_tags.call_args_list[0][1]["Tags"])
+
+    def test_createNonEbsVolumeCheckpoint(self):
+        self.someInstance.block_device_mappings = [
+            { "DeviceName": "/dev/sda", "Ebs": { "VolumeId": self.someVolumeId } },
+            { "DeviceName", "swap" }
+        ]
+        snap = timewarp.ec2.VirtualMachine(self.someInstanceId).create_checkpoint()
+        self.volume.create_snapshot.assert_called_once()
+        self.snapshot.create_tags.assert_called()
 
     def test_createNamedCheckpoint(self):
         self.someInstance.block_device_mappings = [
@@ -58,6 +68,8 @@ class TestEc2Create(unittest.TestCase):
             s.create_tags.return_value = True
             volumes.append(v)
             snapshots.append(s)
+            v.test_device_name = "/dev/xvd{}".format(char)
+            v.test_snapshot = s
             self.someInstance.block_device_mappings.append({
                 "DeviceName": "/dev/xvd{}".format(char), "Ebs": { "VolumeId": v.id }
             })
@@ -66,8 +78,10 @@ class TestEc2Create(unittest.TestCase):
 
         timewarp.ec2.VirtualMachine(self.someInstanceId).create_checkpoint()
 
+        i = 0
         for v in volumes:
             v.create_snapshot.assert_called_once()
+            self.assertTrue({"Key": "timewarp:device", "Value": v.test_device_name} in v.test_snapshot.create_tags.call_args_list[0][1]["Tags"])
         for s in snapshots:
             s.create_tags.assert_called()
 
